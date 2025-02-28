72438834b9eb9652d83fd3424e68f600ec0299f2
gatoartstudio <hackhcctecno@gmail.com>
2025-02-28 18:03:45 -0500

    Archivo para docker, el cual se encarga de traer los cambios de github para mantener el bot actualizado

---
+++ start.sh
@@ -0,0 +1,16 @@
+if [[ -d .git ]] && [[ "1" == "1" ]]; then 
+    echo "[GatoBot] Reseteando cambios locales..." && \
+    git reset --hard HEAD && \
+    echo "[GatoBot] Actualizando desde git..." && \
+    git pull
+fi
+
+if [[ ! -z "" ]]; then 
+    pip install -U --prefix .local
+fi
+
+if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then 
+    pip install -U --prefix .local -r ${REQUIREMENTS_FILE}
+fi
+
+/usr/local/bin/python /home/container/main.py
